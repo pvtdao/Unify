3dd2f3e3c1d1214794a472070e4323e3d3835821
daophan@gmail.com <daophan@gmail.com>
2020-12-10 13:49:33 +0700

    abc

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,39 @@
 //JS here
 
+const animationDuration = 2000;
+const frameDuration = 1000 / 60;
+const totalFrames = Math.round( animationDuration / frameDuration );
+const easeOutQuad = t => t * ( 2 - t );
 
+const animateCountUp = el => {
+	let frame = 0;
+	const countTo = parseInt( el.innerHTML, 10 );
+	const counter = setInterval( () => {
+		frame++;
+		const progress = easeOutQuad( frame / totalFrames );
+		const currentCount = Math.round( countTo * progress );
+
+		if ( parseInt( el.innerHTML, 10 ) !== currentCount ) {
+			el.innerHTML = currentCount;
+		}
+
+		if ( frame === totalFrames ) {
+			clearInterval( counter );
+		}
+	}, frameDuration );
+};
+
+const runAnimations = () => {
+	const countupEls = document.querySelectorAll( '.num' );
+	countupEls.forEach( animateCountUp );
+};
+
+// $('.about').waypoint(function() {
+//     runAnimations()
+// });
+$('.why').waypoint(function() {
+    runAnimations()
+});
 
 document.addEventListener('DOMContentLoaded', event => {
     const buttonMenu = document.querySelector('#btn-menu')
